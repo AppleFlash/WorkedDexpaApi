dfcd0248e73fc7e13e0235de8ddd6d75ebc9207e
Vladislav <sedinkin1993@gmail.com>
2016-05-31 21:47:35 +0600

    First commit

---
+++ Dexpa.WebApi/Controllers/TrackPointsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Dexpa.Core.Model;
+using Dexpa.Core.Services;
+using Dexpa.Core.Utils;
+using Dexpa.DTO;
+using Dexpa.WebApi.Utils;
+
+// Delete if needed
+using Dexpa.Core.Model.Tracks;
+
+namespace Dexpa.WebApi.Controllers
+{
+    public class TrackPointsController : ApiControllerBase
+    {
+        private ITrackPointService mTrackPointService;
+
+        public TrackPointsController(ITrackPointService trackPointService)
+        {
+            mTrackPointService = trackPointService;
+        }
+
+        //public IList<TrackPointDTO> Get(long driverId, DateTime fromDate, DateTime toDate)
+        public TrackData Get(long driverId, DateTime fromDate, DateTime toDate)
+        {
+           /* var fromDateUtc = TimeConverter.LocalToUtc(fromDate);
+            var toDateUtc = TimeConverter.LocalToUtc(toDate);
+            var point = mTrackPointService.GetAggregatedTrackPoints(driverId, fromDateUtc, toDateUtc);
+            var pointDTOs = ObjectMapper.Instance.Map<IList<TrackPoint>, List<TrackPointDTO>>(point);
+            return null;*/
+            return mTrackPointService.GetTrackerData(driverId, fromDate, toDate);
+        }
+
+        public IList<TrackPointDTO> GetDriverPositions(DateTime time)
+        {
+            var timeUtc = TimeConverter.LocalToUtc(time);
+            var point = mTrackPointService.GetDriversPositions(timeUtc);
+            var pointDTOs = ObjectMapper.Instance.Map<IList<TrackPoint>, List<TrackPointDTO>>(point);
+            return pointDTOs;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                mTrackPointService.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+    }
+}
